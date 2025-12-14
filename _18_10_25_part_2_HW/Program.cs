f8be51270283d49397683d0ef5ac5ce88043a7af
piprock <yura.losov@gmail.com>
2025-12-14 14:48:02 +0200

    HW

--- _18_10_25_part_2_HW/Program.cs
+++ _18_10_25_part_2_HW/Program.cs
@@ -2,6 +2,27 @@
 {
     internal class Program
     {
+        static string Task1_InsertStringAt(string original, string toInsert, int idx)
+        {
+            if (idx < 0 || idx > original.Length)
+            {
+                return original;
+            }
+            return original.Substring(0, idx) + toInsert + original.Substring(idx);
+        }
+
+        static bool Task2_IsPalindrome(string text)
+        {
+            for (int i = 0; i < text.Length / 2; i++)
+            {
+                if (text[i] != text[text.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
         static void Task3(string text)
         {
             int uppeCount = 0, lowerCount = 0;
@@ -37,17 +58,39 @@
             Console.WriteLine("Modified words: " + string.Join(", ", words));
         }
 
+        static void CheckIsPalindrome(string text)
+        {
+            Console.WriteLine($"{text} - is{(Task2_IsPalindrome(text) ? "" : " not")} Palindrom");
+        }
+
         static void Main(string[] args)
         {
+            string task1Original = "HelloWorld";
+            string task1ToInsert = "Beautiful";
+            int task1Idx = 5;
+            Console.WriteLine($"Original: {task1Original}\nTo Insert: {task1ToInsert}\nIndex: {task1Idx}");
+            string task1Result = Task1_InsertStringAt(task1Original, task1ToInsert, task1Idx);
+            Console.WriteLine($"Result: {task1Result}");
+            Console.WriteLine();
+
+            string task2_1 = "madam";
+            string task2_2 = "hello";
+            
+            CheckIsPalindrome(task2_1);
+            CheckIsPalindrome(task2_2);
+            Console.WriteLine();
+
             string task3Input = "Hello my World!";
             Console.WriteLine($"\"{task3Input}\"");
             Task3(task3Input);
+            Console.WriteLine();
 
             string[] task4Input = { "example", "test", "ty", "programming", "apple" };
             int countSymbs = 3;
             Console.WriteLine(string.Join(", ", task4Input));
             Console.WriteLine("Count $: " + countSymbs);
             Task4(task4Input, countSymbs);
+            Console.WriteLine();
         }
     }
 }
---
+++ _29_11_25_HW/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _29_11_25_HW
+{
+    public abstract class GeometryFigure
+    {
+        protected abstract double GetArea();
+        protected abstract double GetPerimeter();
+    }
+    public abstract class _3SideFigure : GeometryFigure
+    {
+        public abstract double A { get; set; }
+        public abstract double B { get; set; }
+        public abstract double C { get; set; }
+    }
+    public class Triangle
+    {
+        private double _a;
+
+        public double A
+        {
+            get { return _a; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Side length must be positive");
+                _a = value;
+            }
+        }
+
+        public Triangle(double a, double b, double c)
+        {
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+        }
+    }
+}
