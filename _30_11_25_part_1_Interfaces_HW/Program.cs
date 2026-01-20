38fadb9947c2d9cd1d9371fe5e8883f0486512b8
piprock <yura.losov@gmail.com>
2026-01-20 22:54:16 +0200

    HW

---
+++ _30_11_25_part_1_Interfaces_HW/Program.cs
@@ -0,0 +1,152 @@
+﻿namespace _30_11_25_part_1_Interfaces_HW
+{
+    interface IOutput
+    {
+        void Show();
+        void Show(string info);
+    }
+    interface IMath
+    {
+        int Max();
+        int Min();
+        float Avg();
+        bool Search(int valueToSearch);
+    }
+
+    interface ISort
+    {
+        void SortAsc();
+        void SortDesc();
+        void SortByParam(bool isAsc);
+    }
+
+    class Array : IOutput, IMath, ISort
+    {
+        int[] arr;
+
+        public Array()
+        {
+            arr = new int[0];
+        }
+
+        public float Avg()
+        {
+            if (arr.Length == 0) throw new Exception("Arr is empty");
+
+            float sum = 0f;
+            foreach (var el in arr)
+            {
+                sum += el;
+            }
+            return sum / arr.Length;
+        }
+
+        public int Max()
+        {
+            if (arr.Length == 0) throw new Exception("Arr is empty");
+
+            int max = arr[0];
+            foreach (var el in arr)
+            {
+                max = Math.Max(max, el);
+            }
+            return max;
+        }
+
+        public int Min()
+        {
+            if (arr.Length == 0) throw new Exception("Arr is empty");
+
+            int min = arr[0];
+            foreach (var el in arr)
+            {
+                min = Math.Min(min, el);
+            }
+            return min;
+        }
+
+        public bool Search(int valueToSearch)
+        {
+            bool isFounded = false;
+            foreach (var el in arr)
+            {
+                if (el == valueToSearch)
+                {
+                    isFounded = true;
+                }
+            }
+            return isFounded;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(this.ToString());
+        }
+
+        public void Show(string info)
+        {
+            Console.WriteLine($"{info} - {this.ToString()}");
+        }
+
+        public void Test()
+        {
+            SetByArr(new int[] { 1, 2, 11, 4, 5, 3 });
+            Show();
+            Show("My info");
+
+            Console.WriteLine($"Max {this.Max()}");
+            Console.WriteLine($"Min {this.Min()}");
+            Console.WriteLine($"Avg {this.Avg()}");
+            Console.WriteLine($"Search 2 {this.Search(2)}");
+            Console.WriteLine($"Search 7 {this.Search(7)}");
+
+            SortAsc();
+            Show("Asc");
+            SortDesc();
+            Show("Desc");
+            SortByParam(true);
+            Show("By param (true)");
+
+        }
+
+        public void SetByArr(int[] arr)
+        {
+            this.arr = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                this.arr[i] = arr[i];
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join(" ", arr);
+        }
+
+        public void SortAsc()
+        {
+            arr.Sort();
+        }
+
+        public void SortDesc()
+        {
+            arr.Sort((a, b) => b.CompareTo(a));
+        }
+
+        public void SortByParam(bool isAsc)
+        {
+            if (isAsc) SortAsc();
+            else SortDesc();
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Array arr = new Array();
+            arr.Test();
+        }
+    }
+}
---
+++ _30_11_25_part_2_Standart_Interfaces_HW/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics.Metrics;
+
+namespace _30_11_25_part_2_Standart_Interfaces_HW
+{
+    class CompareByRating : IComparer<Movie>
+    {
+        public int Compare(Movie? x, Movie? y)
+        {
+            if (x == null && y == null) return 0;
+            if (x == null || x.Rating == null) return -1;
+            if (y == null || y.Rating == null) return 1;
+
+            return x.Rating.Value.CompareTo(y.Rating.Value);
+        }
+    }
+
+    class CompareByYear : IComparer<Movie>
+    {
+        public int Compare(Movie? x, Movie? y)
+        {
+            if (x == null && y == null) return 0;
+            if (x == null || x.Year == null) return -1;
+            if (y == null || y.Year == null) return 1;
+
+            return x.Year.Value.CompareTo(y.Year.Value);
+        }
+    }
+
+
+    class Cinema : IEnumerable
+    {
+        Movie[] movies;
+        public string Address { get; set; }
+        public Cinema(string address)
+        {
+            movies = new Movie[0];
+            Address = address;
+        }
+
+        public void Sort()
+        {
+            movies.Sort();
+        }
+        public void Sort(IComparer<Movie> comparer)
+        {
+            movies.Sort(comparer);
+        }
+
+        public void Add(Movie movie)
+        {
+            movies = movies.Append(movie).ToArray();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return movies.GetEnumerator();
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(this.ToString());
+            foreach (Movie movie in movies)
+            {
+                Console.WriteLine(movie.ToString());
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Cinema has {movies.Length} movies";
+        }
+    }
+
+    class Director : ICloneable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Director(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public object Clone()
+        {
+            return new Director(FirstName, LastName);
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+
+    public enum Genre
+    {
+        None = 0,
+        Comedy,
+        Tragedy,
+        Horror,
+        Thriller,
+        Romantic
+    }
+
+    class Movie : ICloneable, IComparable<Movie>
+    {
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public Director? Director { get; set; }
+        public string? Country { get; set; }
+        public Genre? MovieGenre { get; set; }
+        public int? Year { get; set; }
+        public float? Rating { get; set; }
+
+        public Movie(string title)
+        {
+            Title = title;
+        }
+
+        public Movie(string title, string? description, Director? director, string? country, Genre? movieGenre, int? year, float? rating)
+        {
+            Title = title;
+            Description = description;
+            Director = director;
+            Country = country;
+            MovieGenre = movieGenre;
+            Year = year;
+            Rating = rating;
+        }
+
+
+        public object Clone()
+        {
+            return new Movie(Title, Description, (Director?)Director?.Clone(), Country, MovieGenre, Year, Rating);
+        }
+
+        public int CompareTo(Movie? other)
+        {
+            if (other == null) return 1;
+
+            return Title.CompareTo(other.Title);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(" ",
+                Title,
+                Description ?? "No Description",
+                Director?.ToString() ?? "No Director",
+                Country ?? "No Country",
+                MovieGenre?.ToString() ?? "No MovieGenre",
+                Year?.ToString() ?? "No Year",
+                Rating?.ToString() ?? "No Rating");
+        }
+    }
+
+    
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cinema cinema1 = new Cinema("Address 1");
+            cinema1.Add(new Movie("Friday 13th", "Teenagers go to camp", new Director("John", "Pork"), null, Genre.Horror, 1999, 5f));
+            cinema1.Add(new Movie("Trueman", null, new Director("John", "Doe"), null, null, 2000, 5f));
+            cinema1.Add(new Movie("Movie 1", null, null, null, null, 2015, 3f));
+            cinema1.Add(new Movie("Test Movie"));
+            cinema1.Add(new Movie("Movie 2", null, null, null, null, 2035, 1f));
+
+            Movie movie1 = new Movie("Movie 3", null, null, null, null, 1985, 4.5f);
+            cinema1.Add(movie1);
+
+            cinema1.PrintInfo();
+
+            cinema1.Sort();
+            cinema1.PrintInfo();
+
+            cinema1.Sort(new CompareByRating());
+            cinema1.PrintInfo();
+
+            cinema1.Sort(new CompareByYear());
+            cinema1.PrintInfo();
+
+            Movie movie2 = (Movie)movie1.Clone();
+
+            movie2.Director = new Director("test", "Last");
+            cinema1.Add(movie2);
+
+            cinema1.PrintInfo();
+
+
+            
+
+        }
+    }
+}
