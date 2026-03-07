42dab51a356221bc24593c57a7a5f85a783a0cf9
piprock <yura.losov@gmail.com>
2026-03-07 19:53:14 +0200

    HW

--- _13_12_25_part_2_TCPListener_Client_HW/Program.cs
+++ _13_12_25_part_2_TCPListener_Client_HW/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace _13_12_25_part_2_TCPListener_Client_HW
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace _13_12_25_part_2_TCPListener_Client_HW
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TcpClient client = new TcpClient();
+            client.Connect(IPAddress.Parse("127.0.0.1"), 5000);
+            var stream = client.GetStream();
+            Console.WriteLine("Avaible commands: quote, exit.");
+            while (true)
+            {
+                Console.Write("> ");
+                string inp = Console.ReadLine().ToLower();
+                stream.Write(Encoding.UTF8.GetBytes(inp));
+
+
+                if (inp == "quote")
+                {
+                    byte[] buffer = new byte[1024];
+                    int count = stream.Read(buffer);
+                    string answ = Encoding.UTF8.GetString(buffer, 0, count);
+                    Console.WriteLine($"Quote: \"{answ}\"");
+                }
+                else if (inp == "exit")
+                {
+                    Console.WriteLine("Bye!");
+                    stream.Close();
+                    client.Close();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Unknown command.");
+                }
+
+                
+            }
         }
     }
 }
--- _13_12_25_part_2_TCPListener_HW/Program.cs
+++ _13_12_25_part_2_TCPListener_HW/Program.cs
@@ -1,10 +1,84 @@
-﻿namespace _13_12_25_part_2_TCPListener_HW
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace _13_12_25_part_2_TCPListener_HW
 {
     internal class Program
     {
+        static string GetRandQuote()
+        {
+            string[] quotes =
+            {
+                "The only way to do great work is to love what you do.",
+                "Talk is cheap. Show me the code.",
+                "Programs must be written for people to read.",
+                "First, solve the problem. Then, write the code.",
+                "Experience is the name everyone gives to their mistakes.",
+                "In order to be irreplaceable, one must always be different.",
+                "Knowledge is power.",
+                "Stay hungry, stay foolish.",
+                "Simplicity is the soul of efficiency.",
+                "Before software can be reusable it first has to be usable.",
+                "Make it work, make it right, make it fast.",
+                "Any fool can write code that a computer can understand.",
+                "Code never lies, comments sometimes do.",
+                "Fix the cause, not the symptom.",
+                "The best error message is the one that never shows up."
+            };
+            Random random = new Random();
+            return quotes[random.Next(0, quotes.Length)];
+
+        }
+
+        static void HandleClient(TcpClient client)
+        {
+            var stream = client.GetStream();
+            Console.WriteLine($"{DateTime.Now.ToString()} {client.Client.RemoteEndPoint} connected.");
+
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+                int count = stream.Read(buffer, 0, buffer.Length);
+                string answ = Encoding.UTF8.GetString(buffer, 0, count);
+                if (answ == "quote")
+                {
+                    string q = GetRandQuote();
+                    Console.WriteLine($"{DateTime.Now.ToString()} {client.Client.RemoteEndPoint} get quote: \"{q}\"");
+                    stream.Write(Encoding.UTF8.GetBytes(q));
+                }
+                else if (answ == "exit")
+                {
+                    Console.WriteLine($"{DateTime.Now.ToString()} {client.Client.RemoteEndPoint} disconected.");
+                    client.Close();
+                    return;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TcpListener server = new TcpListener(IPAddress.Any, 5000);
+            server.Start();
+
+
+            Console.WriteLine("Server started!");
+            while (true)
+            {
+                Console.WriteLine("Wait for client...");
+                TcpClient client = server.AcceptTcpClient();
+                Task.Run(() =>
+                {
+                    try
+                    {
+                        HandleClient(client);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine($"{DateTime.Now.ToString()} Error occured with {client?.Client?.RemoteEndPoint}");
+                    }
+                });
+            }
         }
     }
 }
